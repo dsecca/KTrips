a8ec4203ec36e0bc4f31fa9cc19f57f235ef35d4
dsecca <davsecc1995@gmail.com>
2017-03-13 13:57:46 -0400

    Wrote the activity_register file.

--- app/src/main/java/test/ktrips/LoginActivity.java
+++ app/src/main/java/test/ktrips/LoginActivity.java
@@ -2,6 +2,7 @@ package test.ktrips;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -23,6 +24,30 @@ public class LoginActivity extends AppCompatActivity {
         btn_signup = (Button) findViewById(R.id.btn_signup);
 
         //Set button listeners
-
+        btn_login.setOnClickListener(onClickbtn_login);
+        btn_signup.setOnClickListener(onClickbtn_signup);
     }
+
+    //Listeners
+    private Button.OnClickListener onClickbtn_login = new Button.OnClickListener(){
+        //Whenever login button is clicked, the method below is called
+        public void onClick(View v){
+            //1. Verify credentials (if have time in sprint 1 do this else in sprint 2)
+            //2. Get a User object
+            //3. Get it from database it in the database
+            //4. Start session
+        }
+    };
+
+    private Button.OnClickListener onClickbtn_signup = new Button.OnClickListener(){
+        //Whenever signup button is clicked, the method below is called
+        public void onClick(View v){
+
+            //1. Verify credentials (if have time in spritn 1 do this else in sprint 2)
+            //2. create an User object
+            //3. put it in the database
+            //4. Start session
+        }
+    };
+
 }
